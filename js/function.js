c8117b1f88cfcb17eb0f17f1c7e3acf66ea0ede3
yyamazaki1732 <125118119+yyamazaki1732@users.noreply.github.com>
2023-03-10 09:07:29 +0900

    second commit

--- js/function.js
+++ js/function.js
@@ -1,28 +1,35 @@
-let count = 0;
 let countCoffee = 0;
 let countTea = 0;
 let countMilk = 0;
 let countCoke = 0;
 let countBeer = 0;
+let countTotal = 0;
 
-let countArray = [countCoffee, countTea, countMilk, countCoke, countBeer]
-const priceArray = [480, 280, 180, 190, 580];
+const countArray = [countCoffee, countTea, countMilk, countCoke, countBeer]
+const counter = document.getElementsByClassName('counter');
 const button = document.getElementsByTagName('button');
-const buttonCounter = document.getElementsByClassName('counter');
-const displayCount = document.getElementById('count');
-const displayPrice = document.getElementById('price');
 
-displayCount.textContent = `商品数：${count} 個`
-displayPrice.textContent = `合計金額：${count * 0} 円`
+const priceArray = [480, 280, 180, 190, 580]
+const countDispay = document.getElementById('count');
+const priceDispay = document.getElementById('price');
 
 for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener('click', addItem);
-  function addItem() {
-    count++;
-    displayCount.textContent = `商品数：${count} 個`
-    displayPrice.textContent = `合計金額：${count * priceArray[i]} 円`
+  button[i].addEventListener('click', function () {
+    countUp();
+  })
 
+  function countUp() {
     countArray[i]++;
-    buttonCounter[i].textContent = countArray[i];
+    counter[i].textContent = countArray[i];
+
+    let priceCoffee = priceArray[0] * countArray[0];
+    let priceTea = priceArray[1] * countArray[1];
+    let priceMilk = priceArray[2] * countArray[2];
+    let priceBeer = priceArray[3] * countArray[3];
+    let priceTotal = priceArray[4] * countArray[4];
+
+    countTotal++;
+    countDispay.textContent = countTotal;
+    priceDispay.textContent = priceCoffee + priceTea + priceMilk + priceBeer + priceTotal
   }
 }
